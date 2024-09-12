1b52945a5b50df6d000be2f9b5e527c42ab7ac68
BUNNY <151705995+chay-666@users.noreply.github.com>
2024-09-12 10:31:54 +0530

    Update Multiplication.java

--- Multiplication.java
+++ Multiplication.java
@@ -6,6 +6,6 @@ public static void main(String args[])
 int a=5;
 int b=10;
 int c=a*b;
-System.out.println("subtraction of"+a+"and"+b+"is"+c);
+System.out.println("Multiplication of"+a+"and"+b+"is"+c);
 }
 }
